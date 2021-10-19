680af5fca324cb3947e1d3f14736bbf3fe99fbaa
AbidemiT <abidemis01@gmail.com>
2021-10-19 10:31:15 +0100

    Update Invesmtent and delete investment feature

--- pages/investment/index.vue
+++ pages/investment/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-fluid mt-5">
-    <div v-if="!investmentForm">
+    <div v-if="!investmentForm && !investmentUpdateForm">
       <card
         class="no-border-card"
         body-classes="px-0 pb-1"
@@ -59,12 +59,12 @@
                   </td>
                   <td>{{ investment.start_date }}</td>
                   <td>{{ investment.end_date }}</td>
-                  <!-- <td>
+                  <td>
                     <el-dropdown trigger="click" class="dropdown">
                       <span
                         class="btn btn-sm btn-icon-only text-light"
                         aria-label="Dropdown menu"
-                        @click="setInvestmentId(investment.id)"
+                        @click="setInvestmentId(investment.id), setInvestment(investment)"
                       >
                         <i class="fas fa-ellipsis-v mt-2"></i>
                       </span>
@@ -76,20 +76,47 @@
                         <a
                           class="dropdown-item"
                           href="#"
+                          @click="investmentUpdateForm = true"
+                          >Update Investment</a
+                        >
+                        <a
+                          class="dropdown-item"
+                          href="#"
                           @click="modals.modal1 = true"
-                          >Approve User</a
+                          >Delete Investment</a
                         >
                       </el-dropdown-menu>
                     </el-dropdown>
-                  </td> -->
+                  </td>
                 </tr>
               </tbody>
             </table>
+            <modal :show.sync="modals.modal1">
+                    <h6
+                      slot="header"
+                      class="modal-title mb-0"
+                      id="modal-title-default"
+                    >
+                      Delete Investment
+                    </h6>
+                    <p>Are you sure you want to delete Investment?</p>
+                    <template slot="footer">
+                      <base-button type="primary" @click="deleteInvestment"
+                        >{{ deleteInvestmentLoading ? 'Deleting Investment...' : 'Delete Investment'}}</base-button
+                      >
+                      <base-button
+                        type="link"
+                        class="ml-auto"
+                        @click="modals.modal1 = false"
+                        >Close
+                      </base-button>
+                    </template>
+                  </modal>
           </div>
         </div>
       </card>
     </div>
-    <div v-else>
+    <div v-else-if="!investmentUpdateForm">
       <div class="row mb-5">
         <div class="col-6">
           <h3 class="mb-0">New Investment</h3>
@@ -346,6 +373,284 @@
                   <div class="col-md-6 text-right">
                     <button class="btn-sm btn-primary">
                       {{
+                        updateInvestmentLoading
+                          ? "Updating Investment"
+                          : "Update Investment"
+                      }}
+                    </button>
+                  </div>
+                  <div class="col-md-6 text-right">
+                    <button
+                      class="btn-sm btn-danger"
+                      @click.prevent="toggleInvestmentForm"
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <div class="row mb-5">
+        <div class="col-6">
+          <h3 class="mb-0">Update Investment</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <form @submit.prevent="updateInvestment">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label
+                    for="investmentName"
+                    class="col-md-6 col-form-label form-control-label"
+                    >Investment Name:</label
+                  >
+                  <div class="col-md-12">
+                    <base-input
+                      type="text"
+                      v-model="investmentUpdateData.name"
+                      required
+                      id="investmentName"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="form-group row">
+                  <label
+                    for="id"
+                    class="col-md-6 col-form-label form-control-label"
+                    >Category ID:</label
+                  >
+                  <div class="col-md-12">
+                    <base-input
+                      type="text"
+                      v-model="investmentUpdateData.investment_category_id"
+                      required
+                      id="id"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="form-group row">
+                  <label
+                    for="example-date-input"
+                    class="col-md-6 col-form-label form-control-label"
+                    >ROI:</label
+                  >
+                  <div class="col-md-12">
+                    <base-input
+                      type="text"
+                      v-model="investmentUpdateData.roi"
+                      required
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label
+                    for="investmentBanner"
+                    class="col-md-6 col-form-label form-control-label"
+                    >Investment Banner:</label
+                  >
+                  <div class="col-md-12">
+                    <base-input
+                      type="text"
+                      v-model="investmentUpdateData.banner_link"
+                      required
+                      id="investmentBanner"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label
+                    for="InvestmentDocument"
+                    class="col-md-6 col-form-label form-control-label"
+                    >Investment Document:</label
+                  >
+                  <div class="col-md-12">
+                    <base-input
+                      type="text"
+                      v-model="investmentUpdateData.document_links"
+                      required
+                      id="InvestmentDocument"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label
+                    for="minimumAmount"
+                    class="col-md-6 col-form-label form-control-label"
+                    >Minimum Amount:</label
+                  >
+                  <div class="col-md-12">
+                    <base-input
+                      type="text"
+                      v-model="investmentUpdateData.minimum_amount"
+                      required
+                      id="minimumAmount"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group row">
+                  <label
+                    for="maximumInvestment"
+                    class="col-md-6 col-form-label form-control-label"
+                    >Maximum Amount:</label
+                  >
+                  <div class="col-md-12">
+                    <base-input
+                      type="text"
+                      v-model="investmentUpdateData.maximum_amount"
+                      required
+                      id="maximumInvestment"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-3">
+                <div class="form-group row">
+                  <label
+                    for="duration"
+                    class="col-md-6 col-form-label form-control-label"
+                    >Duration</label
+                  >
+                  <div class="col-md-12">
+                    <base-input
+                      type="text"
+                      placeholder=" in Months e.g 1 Month, 12 Months"
+                      v-model="investmentUpdateData.duration"
+                      required
+                      id="duration"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="form-group row">
+                  <label
+                    for="investmentStatus"
+                    class="col-md-6 col-form-label form-control-label"
+                    >Status</label
+                  >
+                  <div class="col-md-12">
+                    <el-select
+                      class="select-danger"
+                      placeholder="Select Investment Status"
+                      v-model="investmentUpdateData.status"
+                      required
+                      id="investmentStatus"
+                    >
+                      <el-option
+                        v-for="option in [
+                          { value: 1, label: 'Active' },
+                          { value: 0, label: 'Closed' },
+                        ]"
+                        class="select-danger"
+                        :value="option.value"
+                        :label="option.label"
+                        :key="option.label"
+                      >
+                      </el-option>
+                    </el-select>
+                    <!-- <base-input
+                      type="text"
+                      placeholder=" 1 for Active, 0 for Closed"
+                      v-model="investmentUpdateData.status"
+                      required
+                      id="investmentDescription"
+                    /> -->
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="form-group row">
+                  <label
+                    for="startDate"
+                    class="col-md-6 col-form-label form-control-label"
+                    >Start Date:</label
+                  >
+                  <div class="col-md-12">
+                    <base-input
+                      type="date"
+                      placeholder="Start Date"
+                      id="startDate"
+                      v-model="investmentUpdateData.start_date"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <div class="form-group row">
+                  <label
+                    for="endDate"
+                    class="col-md-6 col-form-label form-control-label"
+                    >End Date:</label
+                  >
+                  <div class="col-md-12">
+                    <base-input
+                      type="date"
+                      placeholder="ENd Date"
+                      id="endDate"
+                      v-model="investmentUpdateData.end_date"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-8">
+                <div class="form-group row align-items-end">
+                  <label
+                    for="investmentDescription"
+                    class="col-md-6 col-form-label form-control-label"
+                    >Description</label
+                  >
+                  <div class="col-md-12">
+                    <textarea
+                      class="form-control"
+                      id="investmentDescription"
+                      rows="3"
+                      placeholder=""
+                      v-model="investmentUpdateData.description"
+                      required
+                    ></textarea>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div
+                  class="
+                    form-group
+                    row
+                    h-50
+                    justify-content-end
+                    align-items-end
+                  "
+                >
+                  <div class="col-md-6 text-right">
+                    <button class="btn-sm btn-primary">
+                      {{
                         newInvestmentLoading
                           ? "Creating New Investment"
                           : "Save New Investment"
@@ -355,7 +660,7 @@
                   <div class="col-md-6 text-right">
                     <button
                       class="btn-sm btn-danger"
-                      @click="toggleInvestmentForm"
+                      @click.prevent="investmentUpdateForm = false"
                     >
                       Cancel
                     </button>
@@ -387,7 +692,6 @@ import {
 export default {
   middleware: "redirect",
   layout: "DashboardLayout",
-
   components: {
     BasePagination,
     [Tooltip.name]: Tooltip,
@@ -400,11 +704,13 @@ export default {
     [Option.name]: Option,
     [Button.name]: Button,
   },
-
   data() {
     return {
       newInvestmentLoading: false,
+      deleteInvestmentLoading: false,
+      updateInvestmentLoading: false,
       investmentForm: false,
+      investmentUpdateForm: false,
       investmentData: {
         name: "",
         start_date: "",
@@ -420,7 +726,23 @@ export default {
         banner_link: "",
         document_links: "",
       },
+      investmentUpdateData: {
+        name: '',
+      description: '',
+      start_date: '',
+      end_date: '',
+      maximum_amount: '',
+      minimum_amount: '',
+      banner_link: '',
+      document_links: '',
+      roi: '',
+      duration: '',
+      status: '',
+      investment_category_id: '',
+      dollar:'0'
+      },
       investments: null,
+      investmentId: '',
       modals: {
         modal1: false,
       },
@@ -430,12 +752,68 @@ export default {
     this.fetchInvestments();
   },
   methods: {
+    setInvestmentId(id) {
+      this.investmentId = id
+    },
+    setInvestment(investment) {
+      // console.log({selectedInvestment: investment});
+      this.investmentUpdateData.name = investment.name
+      this.investmentUpdateData.description = investment.description
+      this.investmentUpdateData.start_date = investment.start_date
+      this.investmentUpdateData.end_date = investment.end_date
+      this.investmentUpdateData.maximum_amount = investment.maximum_amount
+      this.investmentUpdateData.minimum_amount = investment.minimum_amount
+      this.investmentUpdateData.banner_link = investment.banner_link
+      this.investmentUpdateData.document_links = investment.document_links
+      this.investmentUpdateData.roi = investment.roi
+      this.investmentUpdateData.duration = investment.duration
+      this.investmentUpdateData.status = investment.status
+      this.investmentUpdateData.investment_category_id = String(investment.investment_category.id)
+      this.investmentUpdateData.dollar = '0'
+    },
     toggleInvestmentForm() {
       this.investmentForm = !this.investmentForm;
     },
+    async updateInvestment() {
+      let url =
+        `http://209.97.136.114/api/v1/investment/_product/${this.investmentId}`;
+      // let url =
+      //   `https://apiv1.smarthalalinvestorclub.com/api/v1/investment/_product//${this.investmentId}`;
+      this.updateInvestmentLoading = true;
+      this.investmentUpdateForm = false;
+      console.log({ updateData: this.investmentUpdateData });
+
+      try {
+        let response = await this.$axios.post(url, this.investmentUpdateData);
+        console.log({ responseUpdate: response });
+        this.updateInvestmentLoading = false;
+        this.fetchInvestments();
+        this.$notify({
+          type: "success",
+          message: `Investment Updated Successfully`,
+        });
+      } catch (error) {
+        this.updateInvestmentLoading = false;
+        if (error.message) {
+          this.$notify({
+            type: "danger",
+            message: `Oops... ${error.message}`,
+          });
+        }
+
+        if (error.response) {
+          this.$notify({
+            type: "danger",
+            message: `Oops... Error Updating Investment`,
+          });
+        }
+      }
+    },
     async newInvestment() {
       let url =
-        "https://apiv1.smarthalalinvestorclub.com/api/v1/investment/_product";
+        "http://209.97.136.114/api/v1/investment/_product";
+      // let url =
+      //   "https://apiv1.smarthalalinvestorclub.com/api/v1/investment/_product";
       this.newInvestmentLoading = true;
       this.investmentForm = false;
       console.log({ dataData: this.investmentData });
@@ -466,6 +844,41 @@ export default {
         }
       }
     },
+    async deleteInvestment() {
+      let url =
+        `http://209.97.136.114/api/v1/investment/_product/${this.investmentId}`;
+      // let url =
+      //   `https://apiv1.smarthalalinvestorclub.com/api/v1/investment/_product/${this.investmentId}`;
+      this.deleteInvestmentLoading = true;
+      this.modals.modal1 = false;
+      console.log({ dataData: this.investmentData });
+
+      try {
+        let response = await this.$axios.delete(url);
+        console.log({ responseDelete: response });
+        this.deleteInvestmentLoading = false;
+        this.fetchInvestments();
+        this.$notify({
+          type: "success",
+          message: `Investment Deleted Successfully`,
+        });
+      } catch (error) {
+        this.deleteInvestmentLoading = false;
+        if (error.message) {
+          this.$notify({
+            type: "danger",
+            message: `Oops... ${error.message}`,
+          });
+        }
+
+        if (error.response) {
+          this.$notify({
+            type: "danger",
+            message: `Oops... Error Deleting Investment`,
+          });
+        }
+      }
+    },
 
     // setUserId(userId) {
     //   console.log("Okay ooo");
@@ -473,9 +886,9 @@ export default {
     //   this.userId = userId;
     // },
     async fetchInvestments() {
-      let url =
-        "https://apiv1.smarthalalinvestorclub.com/api/v1/investment/_product";
-      //   let url = "http://209.97.136.114/api/v1/investment/_product";
+      // let url =
+        // "https://apiv1.smarthalalinvestorclub.com/api/v1/investment/_product";
+        let url = "http://209.97.136.114/api/v1/investment/_product";
 
       try {
         let response = await this.$axios.get(url);
